349058447bac0f02f07749d14e93e9b3cc3fb190
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-10-10 22:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function threeSum(arr, target) {
 	let min = Number.MAX_VALUE;
 	for(let i=0;i<arr.length-3;i++){
 		let sum = arr[i] + arr[i+1] + arr[i+2];
-		if((sum-target) < min){
+		if((sum-target) < Math.floor(min)){
 			min = sum;
 		}
 	}
