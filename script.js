0892d46f068b94c1f4975f27e10c64432baf54d9
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-10-10 23:28:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function threeSum(arr, target) {
-	let min = 0;
-	
+	let min = Number.MAX_VALUE;
+	let s = 0;
 	for(let i=0;i<arr.length-2;i++){
 		let sum = arr[i] + arr[i+1] + arr[i+2];
 		if(arr.length == 3){
 			return sum;
 		}
 		if((sum-target) < min){
-			min = sum;
+			min = sum-target;
+			s = sum;
 		}
 	}
 	return min;
